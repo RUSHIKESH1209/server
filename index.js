2499e0572767a622a01e696d55e9e8fbd475c4b6
RUSHIKESH1209 <rushikeshchintkuntwar123456@gmail.com>
2025-06-18 11:50:23 +0530

    commmit2

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { connectDB } from "./db.js"; 
+import { connectDB } from "./db.js";
 import schoolRouter from "./schoolRoute.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors(
+  {origin: "*"}
+));
 app.use(express.json());
 
 app.use("/api", schoolRouter);
